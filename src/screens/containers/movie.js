94c17c99bc69cb94013fb8582a9cd8ae645fe335
Johan Mora <joenmodu99@gmail.com>
2020-01-20 10:54:41 -0400

    Bonus animaciones

--- src/screens/containers/movie.js
+++ src/screens/containers/movie.js
@@ -5,8 +5,13 @@ import Header from '../../sections/components/header';
 import Close from '../../sections/components/close';
 import {connect} from 'react-redux';
 import Details from '../../videos/components/details';
-
+import {
+    Animated //Componente para animar
+} from 'react-native';
 class Movie extends Component {
+    state = {
+        opacity: new Animated.Value(0),//instancia del valor que quiero cambiar en este caso instancio que el valor que voy a cambiar es la opacidad
+    }
     closeVideo = () =>{
         this.props.dispatch({
             type: 'SET_SELECTED_MOVIE',
@@ -16,8 +21,23 @@ class Movie extends Component {
         })
     }
 
+    componentDidMount(){
+        Animated.timing( //Función que cambia la opacidad
+            this.state.opacity,
+            {
+                toValue: 1,//Quiero llegar al valor 1
+                duration: 1000,// en un segundo
+            }
+        ).start(); //Comienza
+    }
     render() {
         return (
+            <Animated.View
+            style = {{
+                flex: 1,
+                opacity: this.state.opacity,
+            }}
+            >
             <MovieLayout>
             <Header>
                 <Close
@@ -27,6 +47,8 @@ class Movie extends Component {
             <Player></Player>
             <Details {...this.props.movie}></Details>
             </MovieLayout>
+            </Animated.View>
+
         )
     }
 }

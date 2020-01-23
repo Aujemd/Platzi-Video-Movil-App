d3c56f678ff6941b3d09115086f0efe82194b5f2
Johan Mora <joenmodu99@gmail.com>
2020-01-23 18:31:28 -0400

    Back Button En Android

--- src/app-navigator-with-state.js
+++ src/app-navigator-with-state.js
@@ -1,15 +1,34 @@
-import { connect } from'react-redux';
-import AppNavigator from'./app-navigator';
-import {
-  createReduxContainer,
-} from'react-navigation-redux-helpers';
+import { 
+  createReduxContainer
+} from 'react-navigation-redux-helpers';
+import { connect } from 'react-redux';
+import AppNavigator from './app-navigator';
+import { BackHandler } from "react-native";
+import { NavigationActions } from "react-navigation";
 
-const AppNavigatorWithState = createReduxContainer(AppNavigator, 'root');
-
-function mapStateToProps(state) {
-  return {
-    state: state.navigation
-  }
+class AppNavigatorWithState extends createReduxContainer(AppNavigator, 'root') {
+componentDidMount() {
+  super.componentDidMount && super.componentDidMount();
+  BackHandler.addEventListener('hardwareBackPress', this.onBackButtonPressAndroid);
 }
 
-export default connect(mapStateToProps)(AppNavigatorWithState)
+componentWillUnmount() {
+  BackHandler.removeEventListener('hardwareBackPress', this.onBackButtonPressAndroid);
+  super.componentWillUnmount && super.componentWillUnmount();
+}
+
+onBackButtonPressAndroid = () => {
+  const { dispatch } = this.props;
+  const { state } = this.getCurrentNavigation();
+  // ... do something with state
+  dispatch(NavigationActions.back());
+  return true;
+};
+}
+
+const mapStateToProps = (state) => {
+return {
+  state : state.navigation
+}
+}
+export default connect(mapStateToProps)(AppNavigatorWithState);
